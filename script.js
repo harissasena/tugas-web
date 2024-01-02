a74f793aa9f6497db281df9db9a8a3771bbd6602
harissasena <155231827+harissasena@users.noreply.github.com>
2024-01-03 02:20:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 // menu
 let navbar = document.querySelector('.navbar');
+let searcForm = document.querySelector('#search-from');
+let cartItem = document.querySelector('.cartItem');
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
+    if (searcForm){
     searcForm.classList.remove('active');
+    }
+    if (cartItem){
     cartItem.classList.remove('active');
+    }
 }
 
 window.onscroll = () => {
